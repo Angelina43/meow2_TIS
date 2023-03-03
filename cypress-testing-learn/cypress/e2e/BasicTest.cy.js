e8e56e2a955165b5188e79251b96d86b38909311
angelina43 <bushueva.03@inbox.ru>
2023-03-04 00:29:24 +0700

    Выполненные тесты: 1. Проверка регистрации с невалид данными 2. Проверка регистрации с валид данными 3. Проверка авторизации 4. Смена города 5. Проверка навигации и скролл страницы

--- cypress-testing-learn/cypress/e2e/BasicTest.cy.js
+++
@@ -1,5 +0,0 @@
-describe('Starting With Tests', () => {
-  it('Does 2+2 equal 4?', () => {
-    expect(2 + 2).to.equal(4);
-  });
-});
---
+++ cypress-testing-learn/cypress/e2e/Chitai-gorodTest.cy.js
@@ -0,0 +1,49 @@
+describe('Launches a website', () => {
+    // it('Checking registration with invalid data', () => {
+    //     cy.visit("https://old.chitai-gorod.ru/");
+    //     cy.get(".js__showPopupLogin > span").click();
+    //     cy.get(".popup__auth_tab__register").click();
+    //     cy.get("#user_phone_reg").type('88888888888');
+    //     cy.get("#registration-popup-email").type("email@mail.ru");
+    //     cy.get("#registration-popup-name").type("Name1932");
+    //     cy.get("#registration-popup-surname").type("_Surname");
+    //
+    // });
+    // it('Checking registration with valid data', () => {
+    //     cy.visit("https://old.chitai-gorod.ru/");
+    //     cy.get(".js__showPopupLogin > span").click();
+    //     cy.get(".popup__auth_tab__register").click();
+    //     cy.get("#user_phone_reg").type('89835672189');
+    //     cy.get("#registration-popup-email").type("email1@mail.ru");
+    //     cy.get("#registration-popup-name").type("Имя");
+    //     cy.get("#registration-popup-surname").type("Фамилия");
+    // }
+    // );
+    // it("authorization verification", () => {
+    //     cy.visit("https://old.chitai-gorod.ru/");
+    //     cy.get(".js__showPopupLogin > span").click()
+    //     cy.get("#user_phone").type("9027653191")
+    //     cy.get("#send_sms").click()
+    // });
+
+    // it('Change city', () => {
+    //     cy.visit("https://old.chitai-gorod.ru/");
+    //     cy.get("#city").click();
+    //     cy.get("#city-сhange").click();
+    //     cy.get("#city-search").type('Томск');
+    //     cy.get("[data-city-name=\"Томск, Томская Область\"]").click();
+    // });
+
+    // it('Checking the menu for navigation and page scrolling', () => {
+    //     cy.visit("https://old.chitai-gorod.ru/");
+    //     cy.get('[href="/catalog/books/"] > b').click();
+    //     cy.get('[href="/catalog/kanctovars/"] > b').click();
+    //     cy.get('[href="/catalog/souvenirs/"] > b').click();
+    //     cy.get('[href="/catalog/toys/"] > b').click();
+    //     cy.get('[href="/catalog/hobbies/"] > b').click();
+    //     cy.get('[href="/catalog/sales/"] > b').click();
+    //     cy.get('.header__logo > a > img').click();
+    //     cy.scrollTo(0,1500);
+    //     cy.get('.scrollToTop').click();
+    // });
+});
